43616ecc7f3f65d95cddc49ae6e3d37854e7baab
Scruf <ekozitski@hotmail.com>
2016-02-10 11:10:06 -0800

    Added index page for sign in testing

--- HealthNet/views.py
+++ HealthNet/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
+from .models import Patient
 # Create your views here.
 def index(request):
-    return HttpResponse("you are in the request")
+    users = Patient.objects.all()
+    template = loader.get_template('HealthNet/index.html')
+    context = {
+        'users':users,
+    }
+    return HttpResponse(template.render(context,request))
