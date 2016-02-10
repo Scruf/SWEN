3201abbc846aaaf73f02bd95d1758f439e4bcbe1
Scruf <ekozitski@hotmail.com>
2016-02-09 18:39:19 -0800

    Settings files Changed

--- HealthNet/models.py
+++ HealthNet/models.py
@@ -2,4 +2,14 @@ from __future__ import unicode_literals
 
 from django.db import models
 
+
+
+class  Patient(models.Model):
+    user_name = models.CharField(max_length=50)
+    password = models.CharField(max_length=50)
+
+
+    def __str__(self):
+        return self.user_name+"-"+self.password
+
 # Create your models here.
--- SWEN/settings.py
+++ SWEN/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'HealthNet.apps.HealthnetConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
