77ef2f6a29f40d7a598f0ed41e8a8a8daedd206e
Scruf <ekozitski@hotmail.com>
2016-02-11 14:36:44 -0800

    Home page

--- SWEN/settings.py
+++ SWEN/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
@@ -33,6 +32,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'HealthNet.apps.HealthnetConfig',
     'django.contrib.admin',
+    'twitter_bootstrap',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
