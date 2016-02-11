c2ca65e2fda97d0978bb4ab764eda76cf8b5d7af
Scruf <ekozitski@hotmail.com>
2016-02-11 13:07:08 -0800

    sign In model finished

--- HealthNet/urls.py
+++ HealthNet/urls.py
@@ -1,6 +1,8 @@
-from django.conf.urls import url,include
-from HealthNet import views
+from django.conf.urls import url
+from . import views
 
 urlpatterns =[
     url(r'^$', views.index, name='index'),
+    url(r'^sign/$', views.sign_in, name='sign'),
+
 ]
--- HealthNet/views.py
+++ HealthNet/views.py
@@ -18,14 +18,12 @@ def index(request):
 #Request will be submited to this controller whoch will generate the message
 #on fail it will gennerate fail message
 #on succes it will generate succes
-def success(request):
-    if request.method == 'POST':
-        search_user_name = request.POST.get('username',None)
-        try:
-            user = Patient.objects.get(user_name=search_user_name)
-            html = ("<h3>You are who you are</h3>")
-            return HttpResponse(html)
-        except Patient.DoesNotExist:
-            return HttpResponse("No such cunt")
-    else:
-        print "Hello"
+def sign_in(request):
+    search_id=request.POST.get('username',None)
+    succes_template = loader.get_template('HealthNet/success.html')
+    failure_template = loader.get_template('HealthNet/failure.html')
+    try:
+        user_name = Patient.objects.get(user_name=search_id)
+        return HttpResponse(succes_template.render({"Hello":user_name},request))
+    except Patient.DoesNotExist:
+        return HttpResponse(failure_template.render({"No such":"cunt"},request))
