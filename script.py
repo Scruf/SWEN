dee72ad77f1ee893dd3f17a7c5e75ad94fee8bfb
Scruf <ekozitski@hotmail.com>
2016-02-15 11:13:23 -0800

    JSON files to push to db

--- script.py
+++ script.py
@@ -9,9 +9,12 @@ with  open('doctor.json') as data_file:
 
 
 patient_user_name=[]
+doctor_data=[]
+patient_data =[]
 doctor_user_name=[]
 for name in data:
     patient_user_name.append(name['first_name'][0]+name['last_name'][0])
+    patient_data.append(name)
 
 
 with open('patient.json') as data:
@@ -27,16 +30,30 @@ for patient in patient_user_name:
         counter = counter+1
 
 
-for user_name,patient in zip(patient_user_name,data):
+for user_name,patient in zip(patient_user_name,patient_data):
     with open('patient_data.json','a') as out:
 
         names ={
             'user_name':user_name,
             'first_name':patient['first_name'],
             'last_name':patient['last_name'],
-            'username':user_name
+            'password':str(uuid.uuid1()).split("-")[0],
+            'user_id':str(uuid.uuid1()),
+            'username':user_name,
         }
-        json.dump(names,outf)
+        json.dump(names,out,indent=2)
+
+for user_name,doctor in zip(doctor_user_name,doctor_data):
+    with open ('doctor_data.json','a') as out:
+        name={
+            'user_name':user_name,
+            'first_name':doctor['first_name'],
+            'last_name':doctor['last_name'],
+            'password':str(uuid.uuid1()).split("-")[0],
+            'user_id':str(uuid.uuid1()),
+            'username':user_name,
+        }
+        json.dump(name,out,indent=2)
 # with open('patient.json','a') as out:
 #     for patien in pa
 #
