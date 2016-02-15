a25eff71d60220b71f6595f914d66e44a342b3f5
Scruf <ekozitski@hotmail.com>
2016-02-15 11:38:18 -0800

    Patient model updated

--- HealthNet/models.py
+++ HealthNet/models.py
@@ -1,5 +1,5 @@
 from __future__ import unicode_literals
-
+import uuid
 from django.db import models
 
 
@@ -7,7 +7,9 @@ from django.db import models
 class  Patient(models.Model):
     user_name = models.CharField(max_length=50)
     password = models.CharField(max_length=50)
-
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    user_id = models.UUIDField(primary_key=False,default=uuid.uuid1, editable=False)
 
     def __str__(self):
         return self.user_name+"-"+self.password
