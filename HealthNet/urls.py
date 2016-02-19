94140973e78499947c54f0dc31e95122d45e2fcd
Scruf <ekozitski@hotmail.com>
2016-02-19 08:05:14 -0800

    Login Implemeted

--- HealthNet/urls.py
+++ HealthNet/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns =[
     url(r'^$', views.index, name='index'),
     url(r'^sign/$', views.sign_in, name='sign'),
+    url(r'^sign/thankyou/$', views.thankyou, name='thankyou'),
 
 ]
--- HealthNet/views.py
+++ HealthNet/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse
 from django.template import loader
 from .models import Patient
@@ -19,11 +19,30 @@ def index(request):
 #on fail it will gennerate fail message
 #on succes it will generate succes
 def sign_in(request):
-    search_id=request.POST.get('username',None)
-    succes_template = loader.get_template('HealthNet/success.html')
-    failure_template = loader.get_template('HealthNet/failure.html')
-    try:
-        user_name = Patient.objects.get(user_name=search_id)
-        return HttpResponse(succes_template.render({"Hello":user_name},request))
-    except Patient.DoesNotExist:
-        return HttpResponse(failure_template.render({"No such":"cunt"},request))
+    sign_in_template = loader.get_template('HealthNet/signin.html')
+    users = Patient.objects.all()
+    context={
+        'users':users,
+    }
+    return HttpResponse(sign_in_template.render(context,request))
+
+
+#thank you controller will check if the user is valid
+#In future it will alsoe validate if the login person is a doctor or a Patient
+def thankyou(request):
+    if request.method=='POST':
+        user_name = request.POST.get('username',None)
+        password = request.POST.get('password',None)
+        try:
+            t = Patient.objects.get(user_name=user_name,password=password)
+            context ={
+                'user_name':user_name,
+            }
+            success_template = loader.get_template('HealthNet/success.html')
+            return HttpResponse(success_template.render(context,request))
+        except Patient.DoesNotExist:
+            context = {
+                'user_name':user_name,
+            }
+            failure_template = loader.get_template('HealthNet/failure.html')
+            return HttpResponse(failure_template.render(context,request))
