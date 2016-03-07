afb233f67706efc6e8a88f3d50b12ae8aa1d982a
Scruf <ekozitski@hotmail.com>
2016-03-07 08:55:18 -0800

    Sign Up fixed. Validation for duplicate enteries added

--- HealthNet/views.py
+++ HealthNet/views.py
@@ -43,7 +43,7 @@ def thankyou(request):
             success_template = loader.get_template('HealthNet/success.html')
             return HttpResponse(success_template.render(context,request))
         except Patient.DoesNotExist:
-            return HttpResponse(str(email))
+            return HttpResponse("Patient with this credentials does not exists")
 
 #signup prompts the user to sign up with their name and contact information and to provide
 #a unique username and password for their account
@@ -59,16 +59,51 @@ def signup(request):
 #with the information provided
 def register(request):
     if request.method == 'POST':
-        username = request.POST.get('username',None)
+        try:
+            username = request.POST.get('username',None)
+            try:
+                test_patient = Patient.objects.get(user_name=username)
+            except Patient.DoesNotExist:
+                print "Good"
+        except Patient.MultipleObjectsReturned:
+            return HttpResponse("Patient with the %s exists"%username)
         password = request.POST.get('password',None)
-        first_name = request.POST.get('first_name',None)
-        last_name = request.POST.get('last_name',None)
-        email = request.POST.get('email',None)
-        cell_phone = request.POST.get('cell_phone',None)
+        try:
+            first_name = request.POST.get('first_name',None)
+            last_name = request.POST.get('last_name',None)
+            try:
+                test_patient = Patient.objects.get(first_name=first_name,last_name=last_name)
+            except Patient.DoesNotExist:
+                print "Good"
+        except Patient.MultipleObjectsReturned:
+            return HttpResponse("Patient with this %s and %s is already registered"%(first_name,last_name))
+        try:
+            email = request.POST.get('email',None)
+            try:
+                test_patient = Patient.objects.get(email=email)
+            except Patient.DoesNotExist:
+                print "Good"
+        except Patient.MultipleObjectsReturned:
+            return HttpResponse("User with thisn %s address is already in the system"%email)
+        try:
+            cell_phone = request.POST.get('cell_phone',None)
+            try:
+                test_patient = Patient.objects.get(cell_phone=cell_phone)
+            except Patient.DoesNotExist:
+                print "Good"
+        except Patient.MultipleObjectsReturned:
+            return HttpResponse("User with this cellphone %s number is registered in the system"%cell_phone)
         symptoms = request.POST.get('symptoms',None)
         hospital_name = request.POST.get('hospital',None)
         address = request.POST.get('address',None)
-        insuarance = request.POST.get('insuarance_number',None)
+        try:
+            insuarance = request.POST.get('insuarance_number',None)
+            try:
+                test_patient = Patient.objects.get(insuarance_number=insuarance)
+            except Patient.DoesNotExist:
+                print "Good"
+        except Patient.MultipleObjectsReturned:
+            return HttpResponse("User with this insuarance number %s is already registered in the system"%insuarance_number)
         hospital_keys = []
         hospital = Hospital.objects.all()
         for h in hospital:
