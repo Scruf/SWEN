28af44b2c34ca9ac6627042da8f7aa68d6c0ef61
Scruf <ekozitski@hotmail.com>
2016-05-02 20:14:17 -0400

    Available time API broken

--- HealthNet/static/HealthNet/js/apoitment.js
+++ HealthNet/static/HealthNet/js/apoitment.js
@@ -1,7 +1,12 @@
-$(document).ready(function(){
+  $(document).ready(function(){
 
   $(".hours").click(function(){
     var date = $(".date-input").val();
+    if (date=='' || date.length<1){
+      alert("Date field cannot be left empty");
+      return;
+    }
+
     var doctor_user_name = $(".doctor_name").val();
     var full_date = date.split("-").join("");
     var url = "http://127.0.0.1:8000/HealthNet/api/apoitment/"+doctor_user_name+"/"+full_date;
@@ -15,13 +20,13 @@ $(document).ready(function(){
 
         data.filter(function(el){
           if(el.availbale_hours && el.day && el.month){
-            $("<input type=time class=time>").insertBefore(".hours");
+            $("<input type=time class=time><br>").insertBefore(".hours");
           }
           //if its not in the year and the month is greater
           if (el.availbale_hours && el.day){
-              $("<input type=time class=time>").insertBefore(".hours");
+              $("<input type=time class=time><br>").insertBefore(".hours");
           }
-          
+
         })
       }
     });
