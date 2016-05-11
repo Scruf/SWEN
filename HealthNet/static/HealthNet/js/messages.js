5677d16224338367bb8a393cd54f691a5de128aa
Scruf <ekozitski@hotmail.com>
2016-05-11 03:17:02 -0400

    Added barebone for message api

--- HealthNet/static/HealthNet/js/messages.js
+++ HealthNet/static/HealthNet/js/messages.js
@@ -1,5 +1,62 @@
 
 $(document).ready(function(){
-  var temp = $(".sender_name").val();
-    console.log("Displaying ",temp);
+  function getCookie(c_name) {
+      if (document.cookie.length > 0) {
+          c_start = document.cookie.indexOf(c_name + "=");
+          if (c_start != -1) {
+              c_start = c_start + c_name.length + 1;
+              c_end = document.cookie.indexOf(";", c_start);
+              if (c_end == -1) c_end = document.cookie.length;
+              return unescape(document.cookie.substring(c_start, c_end));
+          }
+      }
+      return "";
+  }
+  var user_name = $(".sender_name").val();
+  var sender = "";
+  var receiver= "";
+  var username = "";
+  var options = {
+    url:"http://127.0.0.1:8000/HealthNet/api/message/"+user_name+"/view/",
+    getValue: function(el){
+      username = el.user_name;
+      sender = el.sender;
+      receiver = el.receiver;
+      return el.name;
+    },
+    list: {
+      match:{
+        enabled:true
+      }
+    },
+  };
+  $('.to').easyAutocomplete(options);
+  $('.send_message').click(function(){
+    if (user_name==username){
+      alert("You cannot send message to yourself");
+      return;
+    }
+    if(username.length==1){
+      alert("Please enter name of the person you'd like to contact");
+      return;
+    }
+    if($(".body-area").val().length==1){
+      alert("Please enter message");
+      return ;
+    }
+    var url = "http://127.0.0.1:8000/HealthNet/api/message/send/";
+    $.ajax({
+      headers:{
+        "X-CSRFToken":getCookie("csrftoken"),
+      },
+      url:url,
+      type:'POST',
+      data:{
+        'sender':user_name,
+        'to':username,
+        'time_stamp':new Date().toString(),
+        'text_body':$(".body-area").val()
+      }
+    })
+  });
 });
